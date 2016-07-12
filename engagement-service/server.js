adae5e8544e1f30e0742c4d4800c9a4798fa73aa
dipo <oladipupo.isola@andela.com>
2016-07-12 13:52:12 +0100

    Eschew surplusage.

--- engagement-service/server.js
+++ engagement-service/server.js
@@ -4,18 +4,6 @@ var server = new grpc.Server();
 var engagementProto = grpc.load('proto/engagement.proto');
 server.addProtoService(engagementProto.Engagement.EngagementService.service, {
   listUsers: function (call, callback) {
-    // needle.get('https://api.github.com/users/andela-gjames/repos?per_page=100',
-    // function (err, reply) {
-    //   callback(null, reply);
-    // });
-    // ({id: 1, name: 'unknown', email: 'email'}).then(function (resources) {
-    //   console.log(resources);
-    //   callback(null, resources);
-    // }).then(function (err) {
-    //   console.log(resources);
-    //   callback(err);
-    // });
-
     callback(null, [{
         id: 1,
         name: "Sade Owolabi",
